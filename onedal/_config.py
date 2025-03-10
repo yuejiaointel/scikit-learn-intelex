5fe8756e8c790976ad07d8305d000182610b0f54
Andreas Huber <9201869+ahuber21@users.noreply.github.com>
2025-03-10 16:49:35 +0100

    chore: update documentation on config parameters (#2340)
    * chore: update documentation on config parameters
    
    * formatting / remove default values
    
    * mention SPMD
    
    * Mention SPMD, formatting
    
    * Warning section and deprecated marker
    
    * fixup
    
    * Update sklearnex/_config.py
    
    Co-authored-by: david-cortes-intel <david.cortes@intel.com>
    
    * Update sklearnex/_config.py
    
    Co-authored-by: david-cortes-intel <david.cortes@intel.com>
    
    * review & cleanup
    
    ---------
    
    Co-authored-by: david-cortes-intel <david.cortes@intel.com>

--- onedal/_config.py
+++ onedal/_config.py
@@ -18,6 +18,30 @@
 
 import threading
 
+"""
+Default values for global configuration parameters.
+These values are typically managed through the sklearnex.set_config() interface.
+Here we only define the defaults.
+
+target_offload:
+    The device primarily used to perform computations.
+    If string, expected to be "auto" (the execution context
+    is deduced from input data location), or SYCL* filter selector string.
+    Global default: "auto".
+allow_fallback_to_host:
+    If True, allows to fallback computation to host device
+    in case particular estimator does not support the selected one.
+    Global default: False.
+allow_sklearn_after_onedal:
+    If True, allows to fallback computation to sklearn after onedal
+    backend in case of runtime error on onedal backend computations.
+    Global default: True.
+use_raw_input:
+    If True, uses the raw input data in some SPMD onedal backend computations
+    without any checks on data consistency or validity.
+    Note: This option is not recommended for general use.
+    Global default: False.
+"""
 _default_global_config = {
     "target_offload": "auto",
     "allow_fallback_to_host": False,
@@ -40,8 +64,8 @@ def _get_config(copy=True):
     Parameters
     ----------
     copy : bool, default=True
-        If False, the values ​​of the global config are returned,
-        which can further be overwritten.
+        If False, a mutable view of the configuration is returned. Each thread
+        has a separate copy of the configuration.
     Returns
     -------
     config : dict
