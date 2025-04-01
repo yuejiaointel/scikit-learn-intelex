4c8dd1a55c4c2c6d0c2ae467be8d5079f6bdbfc7
Ian Faust <icfaust@gmail.com>
2025-04-01 19:00:46 +0200

    [bugfix] repair "releases" azp pipeline (#2394)
    * Update release.yml
    
    * Update gen_release_jobs.py
    
    * Update gen_release_jobs.py
    
    * Update gen_release_jobs.py
    
    * Update release.yml
    
    * formatting
    
    * Update release.yml
    
    * small testing fixes

--- .ci/scripts/gen_release_jobs.py
+++ .ci/scripts/gen_release_jobs.py
@@ -16,6 +16,8 @@
 # ==============================================================================
 
 import argparse
+import os
+import re
 import sys
 
 parser = argparse.ArgumentParser()
@@ -23,26 +25,58 @@ parser.add_argument("--channels", nargs="+", default=["pypi"])
 args = parser.parse_args()
 
 CHANNELS = args.channels
-PYTHON_VERSIONS = ["3.9", "3.10", "3.11"]
 # image versions are pinned to exact number instead of "latest"
 # to avoid unexpected failures when images are updated
-SYSTEMS = ["ubuntu-22.04", "windows-2022"]
 ACTIVATE = {
-    "ubuntu-latest": "conda activate",
-    "windows-latest": "call activate",
+    "ubuntu": "conda activate",
+    "windows": "call activate",
 }
 
 print(CHANNELS)
 
+
+def generate_python_versions(file="README.md"):
+    """Attempt to centralize the supported versions in the right location: the README.
+    Take it from a badge which lists the supported versions."""
+    regex = (
+        r"(?<=\[python version\]\(https://img.shields.io/badge/python-).*(?=-blue\)\])"
+    )
+    sep = "%20%7C%20"
+    pydefaults = ["3.9", "3.10", "3.11"]
+    if os.path.isfile(file):
+        with open(file, "r") as f:
+            pydefaults = re.findall(regex, f.read())[0].split(sep)
+    return pydefaults
+
+
+PYTHON_VERSIONS = generate_python_versions()
+
+
+def collect_azp_CI_OS_images(file=f".ci{os.sep}pipeline{os.sep}ci.yml"):
+    """Attempt to centralize the supported version from the azp CI pipeline, which
+    represents the currently tested versions in Azure Pipelines."""
+    regex = r"(?<=vmImage: ').*(?=')"
+    sysdefaults = ["ubuntu-22.04", "windows-2022"]
+    if os.path.isfile(file):
+        with open(file, "r") as f:
+            # find unique values with set
+            sysdefaults = list(set(re.findall(regex, f.read())))
+    return sysdefaults
+
+
+OS_VERSIONS = collect_azp_CI_OS_images()
+
+
 res_enum = {}
 for channel in CHANNELS:
     for python_version in PYTHON_VERSIONS:
-        for os in SYSTEMS:
-            res_key = channel + " - " + "python" + python_version + " - " + os
+        for vmOS in OS_VERSIONS:
+            res_key = channel + " - " + "python" + python_version + " - " + vmOS
             res_enum[res_key] = {}
             res_enum[res_key]["python.version"] = python_version
-            res_enum[res_key]["imageName"] = os
-            res_enum[res_key]["conda.activate"] = ACTIVATE[os]
+            res_enum[res_key]["imageName"] = vmOS
+            # collect only the OS name, not version via split
+            res_enum[res_key]["conda.activate"] = ACTIVATE[vmOS.split("-")[0]]
             res_enum[res_key]["conda.channel"] = channel
 
 sys.stderr.write("##vso[task.setVariable variable=legs;isOutput=true]{}".format(res_enum))
